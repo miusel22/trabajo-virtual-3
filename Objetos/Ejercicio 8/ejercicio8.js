800ed7ac6ffd7e44d08dab7111751c63bcde9716
camila <vamilaplox@gmail.com>
2020-03-21 00:17:37 -0500

    modificacion

--- Objetos/Ejercicio 8/ejercicio8.js
+++ Objetos/Ejercicio 8/ejercicio8.js
@@ -3,6 +3,11 @@ const button = document.querySelector('button');
 const select = document.querySelector('.select');
 const text = document.querySelector('.emoji');
 
+// var numero=0;;
+// var max=100;
+// var min=0;
+// var numero1=0;
+// var bandera=0;
 
 button.addEventListener('onClick', update);
 
@@ -16,3 +21,24 @@ function update(){
         body.classList.remove('yellow')
     }
 }
+
+
+
+// const boton=document.querySelector('.boton');
+
+
+button.addEventListener('click', () => {
+    if (select.selectedIndex===0){
+        text.innerHTML=':)';
+    }
+    else if(select.selectedIndex===1){
+        text.innerHTML=':(';
+    }   
+});
+
+// function mood(){
+    
+   
+// }
+
+
